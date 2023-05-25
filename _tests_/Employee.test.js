8c9d6c38afbc30d16f121712fc57e00b8d332d8a
ddelgado25 <118149533+ddelgado25@users.noreply.github.com>
2023-05-25 17:27:36 -0400

    Finished Intern.test.js

--- _tests_/Employee.test.js
+++ _tests_/Employee.test.js
@@ -1,7 +1,6 @@
-// Employee constructor 
 const Employee = require('../lib/Employee');
 
-// creates the Employee object 
+// Employee object 
 test('creates an employee object', () => {
     const employee = new Employee('David', 25, 'david@gmail.com', 'david12345');
 
@@ -10,28 +9,28 @@ test('creates an employee object', () => {
     expect(employee.email).toEqual(expect.any(String));
 });
 
-// checks for Employee Name
+// Employee Name
 test('gets employee name', () => {
     const employee = new Employee('David', 25, 'david@gmail.com', 'david12345');
 
     expect(employee.getName()).toEqual(expect.any(String));
 });
 
-// checks for Employee ID 
+// Employee ID 
 test('gets employee ID', () => {
     const employee = new Employee('David', 25, 'david@gmail.com', 'david12345');
 
     expect(employee.getId()).toEqual(expect.any(Number));
 });
 
-// checks for Employee Email
+// Employee Email
 test('gets employee email', () => {
     const employee = new Employee('David', 25, 'david@gmail.com', 'david12345');
 
     expect(employee.getEmail()).toEqual(expect.stringContaining(employee.email.toString()));
 });
 
-// checks for Employee Role
+// Employee Role
 test('gets role of employee', () => {
     const employee = new Employee('David', 25, 'david@gmail.com', 'david12345');
 
